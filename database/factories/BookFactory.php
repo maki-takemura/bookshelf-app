99feffa61e8195364e02356d0b449c6607dab9dc
maki takemura <takemuramaki3@gmail.com>
2026-07-02 20:00:26 +0900

    feat: Factory作成

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Book;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => fake()->word(),
+            'author' => fake()->name(),
+            'isbn' => fake()->unique()->numerify('#############'),
+            'published_date' => fake()->date(),
+            'description' => fake()->paragraph(),
+            'image_url' => fake()->imageUrl(),
+        ];
+    }
+}
