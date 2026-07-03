15b86ee7959ec4493ceb58fb35ba6f63c2860c17
maki takemura <takemuramaki3@gmail.com>
2026-07-03 11:05:28 +0900

    feat: シーダー作成

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $items = [
+            [
+                'name' => '山田太郎',
+                'email' => 'yamada@example.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => '鈴木花子',
+                'email' => 'suzuki@example.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => '田中一郎',
+                'email' => 'tanaka@example.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => '佐藤美咲',
+                'email' => 'sato@example.com',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => '高橋健太',
+                'email' => 'takahashi@example.com',
+                'password' => Hash::make('password'),
+            ],
+        ];
+
+        foreach ($items as $item) {
+            User::firstOrCreate(
+                [
+                    'email' => $item['email'],
+                ],
+                $item
+            );
+        }
+    }
+}
