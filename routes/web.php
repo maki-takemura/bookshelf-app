b9e5e4403e8f05b9bef422b2d9e7f26e6c35d6ce
maki takemura <takemuramaki3@gmail.com>
2026-07-09 00:11:39 +0900

    feat: ジャンルCRUD実装

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\GenreController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// 仮ルート
 Route::get('/', function () {
     return view('welcome');
+})->name('books.index');
+
+Route::get('/books/{book}', function ($book) {
+    return "書籍詳細画面（仮実装）<br>Book ID: {$book}";
+})->name('books.show');
+
+// ランキング（仮）
+Route::get('/ranking', function () {
+    return 'ランキング画面（仮実装）';
+})->name('ranking.index');
+
+// お気に入り一覧（仮）
+Route::get('/favorites', function () {
+    return 'お気に入り一覧（仮実装）';
+})->name('favorites.index');
+
+// 書籍登録（仮）
+Route::get('/books/create', function () {
+    return '書籍登録画面（仮実装）';
+})->name('books.create');
+// 仮ルートここまで
+
+Route::middleware('auth')->group(function () {
+    Route::resource('genres', GenreController::class);
 });
